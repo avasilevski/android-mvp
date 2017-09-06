8a4464a6a656de11adae78636397ff4da3e3b801
Aleksandar <vasilevski85@gmail.com>
2017-09-06 12:14:00 +0200

    *

--- app/src/main/java/com/aleksandarvasilevski/android_mvp/network/RegisterTask.java
+++ app/src/main/java/com/aleksandarvasilevski/android_mvp/network/RegisterTask.java
@@ -2,35 +2,34 @@ package com.aleksandarvasilevski.android_mvp.network;
 
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class LoginTask extends AsyncTask <String, Void, String>{
+public class RegisterTask extends AsyncTask<String, Void, String> {
 
     public IOnTaskComplete delegate = null;
 
-    public LoginTask() {}
+    public RegisterTask(){}
 
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
-        String login_url = "http://192.168.1.108/login.php";
-        if (type.equals("login")){
+        String register_url = "http://192.168.1.108/register.php";
+        if (type.equals("register")){
             try {
                 String email = params[1];
-                String password = params[2];
-                URL url = new URL(login_url);
+                String password = params [2];
+                String firstname = params [3];
+                String lastname = params [4];
+                URL url = new URL(register_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
@@ -38,7 +37,9 @@ public class LoginTask extends AsyncTask <String, Void, String>{
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
                 String postData = URLEncoder.encode("email", "UTF-8")+"="+URLEncoder.encode(email, "UTF-8")+"&"
-                        +URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                        +URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")+"&"
+                        +URLEncoder.encode("firstname", "UTF-8") + "=" + URLEncoder.encode(firstname, "UTF-8")+"&"
+                        +URLEncoder.encode("lastname", "UTF-8") + "=" + URLEncoder.encode(lastname, "UTF-8");
                 bufferedWriter.write(postData);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -52,15 +53,10 @@ public class LoginTask extends AsyncTask <String, Void, String>{
                 }
                 bufferedReader.close();
                 inputStream.close();
-                Log.i("#LOG", "Result is: " + result);
                 return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e){
-                e.printStackTrace();
+            }catch (Exception e){
             }
         }
-        Log.i("#LOG", "result is null");
         return null;
     }
 
---
+++ app/src/main/java/com/aleksandarvasilevski/android_mvp/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aleksandarvasilevski.android_mvp.ui;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aleksandarvasilevski.android_mvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
